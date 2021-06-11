e8d0566f06fd66c441c3979e944bf7d488eaef45
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-06-11 08:22:56 +0200

    Add files via upload

---
+++ KaffemaskinenDel3/KaffemaskinenDel3/LiquidContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffemaskinenDel1
+{
+    class LiquidContainer
+    {
+        public enum liquids { water = 1, }
+
+        public string PourLiquid(liquids liquid)
+        {
+            if (liquid.Equals(liquids.water))
+            {
+                return "Adding water to container";
+            }
+            else
+            {
+                return "Invalid liquid inserted";
+            }
+        }
+    }
+}
---
+++ KaffemaskinenDel3/KaffemaskinenDel3/Machine.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaffemaskinenDel1
+{
+    public abstract class Machine
+    {
+        public abstract string Start();
+        public abstract string Stop();
+    }
+}
